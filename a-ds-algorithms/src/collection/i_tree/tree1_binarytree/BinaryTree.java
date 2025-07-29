6c857614d674909fa7d2075a6ea3afc5ef347334
JieHFUT <9594523wrj@gmail.com>
2025-07-29 17:50:20 +0800

    review

--- a-ds-algorithms/src/collection/i_tree/tree1_binarytree/BinaryTree.java
+++ a-ds-algorithms/src/collection/i_tree/tree1_binarytree/BinaryTree.java
@@ -107,10 +107,43 @@ public class BinaryTree {
     // 11.将一棵树的每一个节点的左右子树指向反过来
     // 12.找到两个指定节点的最近的公共祖先
 
+    // 获取树中节点的个数
+    public int numberOfNodes() {
+        return numberOfNodes(root);
+    }
+    public int numberOfNodes(Node root) {
+        if (root == null) return 0;
+        return 1 + numberOfNodes(root.getLeft()) + numberOfNodes(root.getRight());
+    }
 
     // 1.获取树中叶子节点的个数
-    public int numberOfNodes() {
+    public int numberOfLeaves() {
+        return numberOfLeaves(root);
+        // return numberOfLeaves(root);
+    }
+    public int numberOfLeaves(Node root) {
+        if (root == null) return 0;
+        if (root.getLeft() == null && root.getRight() == null) return 1;
+        if (root.getLeft() == null) return numberOfLeaves(root.getRight());
+        if (root.getRight() == null) return numberOfLeaves(root.getLeft());
+        // 左右子树都不为空
+        return numberOfLeaves(root.getLeft()) + numberOfLeaves(root.getRight());
+    }
 
+    int recodeLeaves = 0;
+    public int numberOfLeaves2(Node root) {
+        if (root == null) return 0;
+        if (root.getLeft() == null && root.getRight() == null) recodeLeaves++;
+        if (root.getLeft() == null) numberOfLeaves(root.getRight());
+        if (root.getRight() == null) numberOfLeaves(root.getLeft());
+        return recodeLeaves;
+    }
+
+
+
+    // 2.获取第 K 层节点的个数
+    public int numberOfNodesOfKLevel(int k) {
+        
     }
 
 
