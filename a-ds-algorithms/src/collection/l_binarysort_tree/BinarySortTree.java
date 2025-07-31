05fde852b306297474d7d1c4636e107c5b08914c
JieHFUT <9594523wrj@gmail.com>
2025-07-31 15:16:33 +0800

    review

--- a-ds-algorithms/src/collection/l_binarysort_tree/BinarySortTree.java
+++ a-ds-algorithms/src/collection/l_binarysort_tree/BinarySortTree.java
@@ -16,6 +16,59 @@ public class BinarySortTree {
     public Node getRoot() {return root;}
 
 
+    // 前序遍历该节点（根-左-右）
+    public void preOrder() {
+        if (root == null) return;
+        root.preOrder(root);
+    }
+
+    // 层序遍历
+    public void levelOrder() {
+        if (root == null) return;
+        root.levelOrder();
+    }
+
+    /**
+     * 向一颗排序树中添加一个节点
+     * @param node
+     */
+    public void add(Node node) {
+        if (node == null) return;
+        if (root == null) root = node;
+        root.add(node);
+    }
+
+    /**
+     * 查找节点
+     * @param key
+     * @return
+     */
+    public Node search(int key) {
+        if (root == null) return null;
+        return root.search(key);
+    }
+
+
+    /**
+     * 返回该节点的父亲节点
+     * @param key
+     * @return
+     */
+    public Node searchParent(int key) {
+        if (root == null) return null;
+        return root.searchParent(key);
+    }
+
+
+    /**
+     * 删除某一个节点，删除完成后还需要保证是排序二叉树
+     * @param key
+     * @return
+     */
+    public boolean delete(int key) {
+        
+    }
+
 
 
 
--- a-ds-algorithms/src/collection/l_binarysort_tree/Node.java
+++ a-ds-algorithms/src/collection/l_binarysort_tree/Node.java
@@ -8,6 +8,7 @@ import java.util.Queue;
  * Package: binarysorttree
  * Description:
  * 二叉排序树
+ * 一个非叶子节点的左子节点总是比该节点小，右子节点总是比该节点大
  * @Author jieHFUT
  * @Create 2024/11/1 22:09
  * @Version 1.0
@@ -38,5 +39,154 @@ public class Node implements Comparable<Node>{
     public String toString() {
         return "Node [value=" + value + "]";
     }
+
+
+    /**
+     * 前序遍历该二叉排序树
+     * @param node
+     */
+    public void preOrder(Node node) {
+        System.out.println(node);
+        if (node.left != null)
+            preOrder(node.left);
+        if (node.right != null)
+            preOrder(node.right);
+    }
+
+
+    /**
+     * 向一个二叉排序树中添加一个节点，添加完成后需要保证仍然是一个二叉排序树
+     * 根节点 root && node 节点都不为 null
+     * @param node 要被添加的节点
+     */
+    public void add(Node node) {
+
+        if (this.compareTo(node) < 0) {
+            // 向右子树看
+            if (this.right == null)
+                this.right = node;
+            else
+                this.right.add(node);
+        } else {
+            // 向左子节点查看
+            if (this.left == null)
+                this.left = node;
+            else
+                this.left.add(node);
+        }
+    }
+
+
+    /**
+     * 对该二叉排序树进行层序遍历
+     */
+    public void levelOrder() {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(this);
+        while (!queue.isEmpty()) {
+            Node peek = queue.peek();
+            if (peek.left != null)
+                queue.add(peek.left);
+            if (peek.right != null)
+                queue.add(peek.right);
+            System.out.println(peek);
+            queue.poll();
+        }
+    }
+
+
+    /**
+     * 查找节点
+     * 二叉排序树
+     * @param value
+     * @return
+     */
+    public Node search(int value) {
+        // 1.如果当前节点就是要查找的节点
+        if (this.value == value)
+            return this;
+        // 2.如果当前节点的数值比 value 大
+        if (this.value > value && this.left != null) {
+            // 向左边找
+            return this.left.search(value);
+        }
+        if (this.value < value && this.right != null) {
+            return this.right.search(value);
+        }
+        return null;
+    }
+
+
+    /**
+     * 寻找该节点的父亲节点
+     * 没有父亲节点或者没有找到该节点就返回 null
+     * @param value
+     * @return
+     */
+    public Node searchParent(int value) {
+        // 思路：采用双指针法
+        Node prev = null;
+        Node root = this;
+        boolean found = false;
+
+        while (true) {
+            if (root.value == value) {
+                // 找到了该节点
+                found = true;
+                break;
+            }
+            if (root.value > value && root.left != null) {
+                prev = root;
+                root = root.left;
+            } else if (root.value < value && root.right != null) {
+                prev = root;
+                root = root.right;
+            } else {
+                // 整个排序二叉树就没有该节点
+                break;
+            }
+
+        }
+        // 在这个位置有两种情况
+        // 1.匹配到该节点，返回 prev 就是其父亲节点
+        // 2.整个排序二叉树中没有该节点
+        if (found) {
+            return prev;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 通过递归的方法寻找其父亲节点
+     * @param value
+     * @return
+     */
+    public Node searchParent2(int value) {
+
+        if (this.value == value) return null;
+
+        if ((this.left != null && this.left.value == value)
+                || (this.right != null && this.right.value == value)) {
+            // 决定了递归时候 if (this.value == value) return null; 不会生效
+            return this;
+        } else {
+            if (this.left != null && this.left.value > value) {
+                // 向左寻找
+                return this.left.searchParent2(value);
+            } else if (this.right != null && this.right.value < value) {
+                return this.right.searchParent2(value);
+            } else {
+                return null;
+            }
+        }
+
+    }
+
+
+
+
+
+
 
 }
