6d47df9c83156a0afe86b2356c1c6b1948bf03f6
JieHFUT <9594523wrj@gmail.com>
2025-09-16 23:58:28 +0800

    review

---
+++ a-ds-algorithms/src/test/NietySeven.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * ClassName: NietySeven
+ * Package: test
+ * Description:
+ *
+ * @Author jieHFUT
+ * @Create 2025/9/16 22:13
+ * @Version 1.0
+ */
+public class NietySeven {
+
+    public static void main(String[] args) {
+        System.out.println("12345".substring(1));
+    }
+}
