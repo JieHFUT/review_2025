6e61b6706ec221fec745be5162c82a3ad43349da
JieHFUT <9594523wrj@gmail.com>
2025-10-02 23:13:00 +0800

    review

--- c-maven/e-maven-trans-a/src/test/java/com/jiehfut/TransTest.java
+++ c-maven/e-maven-trans-a/src/test/java/com/jiehfut/TransTest.java
@@ -6,8 +6,10 @@ public class TransTest {
 
 
     /**
-     b 依赖了 druid && junit && servlet，而 a 依赖了 b
-            compile   test    provided
+     b 依赖了 druid && junit && servlet，
+     a 依赖了 b
+
+     compile   test    provided
      并不是所有的依赖都能依赖传递
      compile 可以传递，test && provided 不能传递
      不能传递的依赖需要自己配置依赖
