c2f7fd1a9b3f58a16a0774ca1a2a1407299f7407
JieHFUT <9594523wrj@gmail.com>
2025-10-06 23:54:22 +0800

    review

--- d-spring/spring6_code/a-spring-example/src/main/java/com/jiehfut/xml/TestUser.java
+++ d-spring/spring6_code/a-spring-example/src/main/java/com/jiehfut/xml/TestUser.java
@@ -51,6 +51,7 @@ public class TestUser {
         logger.info("logger 调用成功！！！！！！！！！！！！！");
 
 
+
         /**
          * 2024-12-26 08:46:13 224 [main] DEBUG org.springframework.context.support.ClassPathXmlApplicationContext - Refreshing org.springframework.context.support.ClassPathXmlApplicationContext@239b0f9d
          * 2024-12-26 08:46:13 380 [main] DEBUG org.springframework.beans.factory.xml.XmlBeanDefinitionReader - Loaded 1 bean definitions from class path resource [bean.xml]
